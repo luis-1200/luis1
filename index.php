6c5e00097167f5e8cbf5946f3c3c420f910b97cd
luis-1200 <117422312+luis-1200@users.noreply.github.com>
2022-11-16 23:38:07 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>.: VER, AGREGAR, ACTUALIZAR Y ELIMINAR :.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2>[CRUD] CREATE, RETRIEVE, UPDATE &amp; DELETE</h2>
+			<p class="lead">Ejemplo de ver, agregar, actualizar, eliminar con PHP y MySQL</p>
+			<p>Instrucciones:</p>
+			<ol>
+			</ol>
+			<br>
+			<ul type="none">
+			<li><i class="glyphicon glyphicon-ok"></i> Facil de instalar y modificar</li>
+			<li><i class="glyphicon glyphicon-ok"></i> Ideal para tu proximo proyecto web</li>
+			</ul>
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
